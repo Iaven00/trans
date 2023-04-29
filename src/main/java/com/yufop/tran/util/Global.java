e6dcf7258d33106e5cc90304c084bdb7149adbbc
Iaven00 <1245325661@qq.com>
2023-04-29 16:58:19 +0800

    user表更新

--- src/main/java/com/yufop/tran/util/Global.java
+++ src/main/java/com/yufop/tran/util/Global.java
@@ -4,7 +4,7 @@ public class Global {
 
     public static final int SUCCESS = 1;
     public static final int FAIL = 0;
-    public static final int CODEERROR = 404;  // 验证码错误
+    public static final int CODEERROR = 105;  // 验证码错误
     public static final int REGISTER = 100; //注册
     public static final int FIND = 101; //找回
     public static final int MODIFY = 102; //修改密码
